09fa351efaf5af55df8e65c78d3848fe6d76955c
biennguyencong25032007-hub <biennguyencong25032007@gmail.com>
2026-03-24 14:17:59 +0700

    Basic

--- Basic/banking.py
+++ Basic/banking.py
@@ -1,23 +1,34 @@
 def show_balance(balance):
+    print("**************************")
     print(f"your balace is ${balance:2f}")
-
-def deposit():
-    amount = float(input("enter an amount to be deposited: "))
+    print("**************************")
     
+def deposit():
+    print("****************************")
+    amount = float(input("enter an amount to be deposited: "))
+    print("****************************")
     if amount < 0:
+        print("**************************")
         print("that is not a valid amount")
+        print("**************************")
         return 0
     else:
         return amount
 
 def withdraw(balance):
+    print("**************************")
     amount = float(input("amount must be greater than 0"))
+    print("**************************")
     
     if amount > balance:
+        print("**************************")
         print("insufficient funds")
+        print("**************************")
         return 0
     elif amount < 0:
+        print("**************************")
         print("amount must be greater than 0")
+        print("**************************")
         return 0
     else:
         return amount
@@ -28,12 +39,16 @@ def main():
     is_running = True
 
     while is_running:
-        print("banking program")
+        print("*********************")
+        print("   banking program   ")
+        print("*********************")
+
         print("1.show balance")
         print("2.deposit")
         print("3.withdraw")
         print("4.exit")
-    
+        print("*********************")
+        
         choice = input("enter your choice (1-4): ")
     
         if choice == '1':
@@ -45,10 +60,14 @@ def main():
         elif choice == '4':
             is_running = False
         else:
-             print("that is not a valid choice")
-        
+            print("**************************")
+            print("that is not a valid choice")
+            print("**************************")
+            
+    print("***************************")        
     print("thank yoU! have a nice day!")
-
+    print("***************************")
+    
 if __name__ == "__name__":
    main()  
     
