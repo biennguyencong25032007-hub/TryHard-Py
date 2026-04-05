eaf95ca529e36631ce20cc707d5f91e9a2c9b5e2
biennguyencong25032007-hub <biennguyencong25032007@gmail.com>
2026-04-06 01:31:28 +0700

    ,

--- test/test.py
+++ test/test.py
@@ -1,11 +1,1 @@
-for item in range:
-    if item == 5:
-        print("Found 5!")
-    else:
-        print("Not 5.")
-        if item == 10:
-            print("Found 10!")
-            
-from django.utils.translation import ugettext_lazy as _
-fiwe = _("Hello, World!")
-    
+print(max(a:=list(map(int,input().split()))), min(a))
